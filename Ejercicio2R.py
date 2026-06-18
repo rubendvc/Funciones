e5c0cc5f475062fa9e2b9ea29c68242a9acdc403
rubendvc <rubendvc@gmail.com>
2026-06-17 21:23:17 -0500

    Ejercicio 2 suma

---
+++ Ejercicio2R.py
@@ -0,0 +1,11 @@
+# Ejercicio 2
+
+def suma(n1, n2):
+    return n1 + n2
+
+n1 = int(input("Ingresa un numero: "))
+n2 = int(input("Ingresa el segundo numero: "))
+
+print(f"El resultado es: {suma(n1, n2)}")
+
+
