c3ee676e2cfb977034b2df741df3feda58429705
Manuel Vargas <manuelevc94@hotmail.com>
2026-06-17 23:45:54 -0500

    Add files via upload
    Función para mostrar saludo sin RETURN.

---
+++ Ej1.py
@@ -0,0 +1,6 @@
+#Crea una función void que reciba un nombre y muestre un saludo
+#en pantalla. No debe retornar ningún valor.
+def saludo(nombre):
+    print(f"Hola {nombre}, ¡Bienvenido, a la semana 5!")
+
+saludo("Manuel")
