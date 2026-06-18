2d3cbf7b411ddac5129c295dd302c5f9f165bcab
rubendvc <rubendvc@gmail.com>
2026-06-17 21:24:11 -0500

    Ejercicio 3 Demostracion

---
+++ Ejercicio3R.py
@@ -0,0 +1,14 @@
+# Ejercicio 3
+
+def area(l):
+    resultado = l * l
+    return resultado
+
+l = int(input("Ingresa el lado del cuadrado: "))
+
+print(f"El area del cuadrado es: {area(l)}")
+
+print(f"El area del cuadrado es {resultado}")
+
+
+
